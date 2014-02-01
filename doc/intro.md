3407cbf16b7faa2d1c30536b8d5d477a592440f9
Nikita Dudnik aka Nek <nikdudnik@gmail.com>
2014-02-01 17:48:02 +0700

    Remove obsolete folders

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to music-finder
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/music_finder/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns music-finder.core-test
-  (:require [clojure.test :refer :all]
-            [music-finder.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
